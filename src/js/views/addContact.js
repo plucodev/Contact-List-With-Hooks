a09b3bd2332785b0a09c9c5be09fd9199f8409ae
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-17 18:30:56 +0000

    added Consumer to addContact.js

--- src/js/views/addContact.js
+++ src/js/views/addContact.js
@@ -24,62 +24,66 @@ export const AddContact = () => {
 		console.log("store:", store);
 	}
 	return (
-		<div className="container">
-			<div className="container">
-				<div>
-					<h1 className="text-center mt-5">Add a new contact</h1>
-					<form>
-						<div className="form-group">
-							<label>Full Name</label>
-							<input
-								value={name}
-								onChange={handleNameChange}
-								type="text"
-								className="form-control"
-								placeholder="Full Name"
-							/>
+		<Consumer>
+			{({ store, actions }) => {
+				return (
+					<div className="container">
+						<div>
+							<h1 className="text-center mt-5">Add a new contact</h1>
+							<form>
+								<div className="form-group">
+									<label>Full Name</label>
+									<input
+										value={name}
+										onChange={handleNameChange}
+										type="text"
+										className="form-control"
+										placeholder="Full Name"
+									/>
+								</div>
+								<div className="form-group">
+									<label>Email</label>
+									<input
+										value={email}
+										onChange={handleEmailChange}
+										type="email"
+										className="form-control"
+										placeholder="Enter email"
+									/>
+								</div>
+								<div className="form-group">
+									<label>Address</label>
+									<input
+										value={address}
+										// THIS IS ANOTHER WAY
+										// IN THIS CASE WE JUST USE 1 LINE TO HAVE THE SAME RESULT
+										onChange={e => setAddress(e.target.value)}
+										type="text"
+										className="form-control"
+										placeholder="Enter address"
+									/>
+								</div>
+								<div className="form-group">
+									<label>Phone</label>
+									<input
+										value={phone}
+										onChange={e => setPhone(e.target.value)}
+										type="text"
+										className="form-control"
+										placeholder="Enter phone"
+									/>
+								</div>
+								<button onClick={() => pass} type="button" className="btn btn-primary form-control">
+									save
+								</button>
+								<Link className="mt-3 w-100 text-center" to="/">
+									or get back to contacts
+								</Link>
+							</form>
 						</div>
-						<div className="form-group">
-							<label>Email</label>
-							<input
-								value={email}
-								onChange={handleEmailChange}
-								type="email"
-								className="form-control"
-								placeholder="Enter email"
-							/>
-						</div>
-						<div className="form-group">
-							<label>Address</label>
-							<input
-								value={address}
-								// THIS IS ANOTHER WAY
-								// IN THIS CASE WE JUST USE 1 LINE TO HAVE THE SAME RESULT
-								onChange={e => setAddress(e.target.value)}
-								type="text"
-								className="form-control"
-								placeholder="Enter address"
-							/>
-						</div>
-						<div className="form-group">
-							<label>Phone</label>
-							<input
-								value={phone}
-								onChange={e => setPhone(e.target.value)}
-								type="text"
-								className="form-control"
-								placeholder="Enter phone"
-							/>
-						</div>
-						<button onClick={() => pass} type="button" className="btn btn-primary form-control">
-							save
-						</button>
-						<Link className="mt-3 w-100 text-center" to="/">
-							or get back to contacts
-						</Link>
-					</form>
-				</div>
-			</div>
-		</div>
+					</div>
+				);
+			}}
+		</Consumer>
 	);
 };
