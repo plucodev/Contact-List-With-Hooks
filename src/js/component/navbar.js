ca08cd476c288a7069171d5a0d5f5febc62f6554
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-21 17:40:22 +0000

    added login button

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -9,8 +9,8 @@ export class Navbar extends React.Component {
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="/login">
+						<button className="btn btn-primary">Login</button>
 					</Link>
 				</div>
 			</nav>
